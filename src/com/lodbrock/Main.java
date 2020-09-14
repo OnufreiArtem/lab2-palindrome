0a1667b0ff2bf77cea8e2602c506fc2862e7956f
OnufreiArtem <2hilderoy2@gmail.com>
2020-09-14 19:27:41 +0300

    added polindrome detection code

--- src/com/lodbrock/Main.java
+++ src/com/lodbrock/Main.java
@@ -1,8 +1,73 @@
 package com.lodbrock;
 
+import static java.lang.Character.isLetter;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        String[] testLines = new String[] {
+                "mamam",
+                "mama.m",
+                "Mamam",
+                "m am amam",
+                "шалаш",
+                "sdfsdf",
+                "Аргентина манит негра",
+                "   ",
+                "a",
+                "\"joj\"",
+                "",
+                "Лепс спел"
+        };
+
+        for(String line : testLines){
+            String result = checkPalindrome(line) ? "is palindrome." : "isn't palindrome.";
+            System.out.println(String.format("'%s' - %s", line, result));
+        }
+
     }
+
+    public static boolean checkPalindrome(String text){
+        if(text == null || text.equals("")) return false;
+
+        int lCursor = 0, rCursor = text.length()-1; // set cursors
+        boolean isPalindrome = true;
+        boolean containsLetters = false;
+
+        text = text.toLowerCase();
+
+        while(lCursor < rCursor){
+            //move right while is not letter
+            while( !isLetter(text.charAt(lCursor)) ) {
+                lCursor++;
+                if(lCursor >= rCursor){
+                    isPalindrome = containsLetters;
+                    break;
+                }
+            }
+            //move left while is not letter
+            while ( !isLetter(text.charAt(rCursor)) ){
+                rCursor--;
+                if(lCursor >= rCursor){
+                    isPalindrome = containsLetters;
+                    break;
+                }
+            }
+
+            containsLetters = true; // true if contains letters
+
+            //check if characters are equal
+            if(text.charAt(lCursor) != text.charAt(rCursor)){
+                isPalindrome = false;
+                break;
+            }
+
+            lCursor++; // move left cursor right
+            rCursor--; // move right cursor left
+        }
+
+        return isPalindrome;
+    }
+
 }
